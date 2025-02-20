20d85511c0441a6b5055569ac11eb1b80e8f5307
hannescato <ditt@example.com>
2025-02-20 15:32:22 +0100

    sending packet testing

--- Host_A.c
+++ Host_A.c
@@ -1,9 +1,28 @@
 #include "Sim_Engine.h"
+#include <string.h>
+#include <stdio.h> 
 
 
 /* Called from layer 5, passed the data to be sent to other side */
 void A_output( struct msg message) {
-  /* TODO */
+
+  struct pkt testpacket;
+
+  testpacket.seqnum = 0;
+  testpacket.acknum = 0;
+
+
+
+  
+  memcpy(testpacket.payload, message.data, sizeof(message.data));
+  
+  tolayer3(0, testpacket);
+
+  printf("sending packet  %d, %s", testpacket.seqnum, testpacket.payload);
+
+
+  
+
 }
 
 /* Called from layer 3, when a packet arrives for layer 4 */
@@ -19,5 +38,6 @@ void A_timerinterrupt() {
 /* The following routine will be called once (only) before any other */
 /* Host A routines are called. You can use it to do any initialization */
 void A_init() {
-  /* TODO */
+  
+
 }
